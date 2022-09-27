2a229553713f8e7251879f0b98ac4fd41726d1f7
YCK1130 <ken1130yang@gmail.com>
2022-09-27 12:04:52 +0800

    basic config done, add close&add butt., js function 0%

---
+++ hw2/main.js
@@ -0,0 +1,13 @@
+var participant_data = [
+    {
+        first_name:"人員1",
+        last_name:"人員1",
+        id:"1",
+        img_url:none,
+    },
+]
+
+var color_pool = ["rgb(0, 89, 255)","rgb(114, 86, 141)","rgb(2, 136, 83)","rgb(181, 175, 5)","rgb(11, 161, 6)"];
+
+
+
